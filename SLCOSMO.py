6d0866df56bafb5ee0e79340bcdc58877383ae09
astroskylee <astro_skylee@qq.com>
2026-01-21 16:26:19 +0000

    Delete SLCOSMO wrapper

--- SLCOSMO.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility wrapper for the refactored SLCOSMO modules."""
-
-from slcosmo.models import SLCOSMO, SLmodel
-from slcosmo.tools import tool
-
-__all__ = ["SLCOSMO", "SLmodel", "tool"]
