9b9147ddebc65673df125b830c0c7f23406479ba
astroskylee <astro_skylee@qq.com>
2026-02-02 16:22:28 +0000

    Inline HMC batch runner and remove wrapper

---
+++ run_hmc_all_gpu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=2000lens
+#SBATCH --time=120:00:00
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=48
+#SBATCH --output=log/output_log%j
+#SBATCH --error=log/error_log%j
+#SBATCH --mail-user=tian.li@port.ac.uk
+#SBATCH --mail-type=ALL
+#SBATCH --partition=gpu.q
+#SBATCH --exclude gpu[01,02]
+
+module load system
+module load anaconda3/2024.02
+echo `module list`
+
+source /mnt/lustre2/shared_conda/envs/tianli/herculens_tian/bin/activate
+cd /users/tianli/LensedUniverse
+
+python -u hmc_scripts/run_lens_kin_hmc.py
+python -u hmc_scripts/run_dspl_hmc.py
+python -u hmc_scripts/run_sne_hmc.py
+python -u hmc_scripts/run_quasar_hmc.py
+python -u hmc_scripts/run_joint_hmc.py
