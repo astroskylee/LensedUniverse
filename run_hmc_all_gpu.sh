b4586a210b23c470b0f512583024bf568a09e7aa
Tian Li <tianli@gpu02.pri.sciama.icg.port>
2026-02-13 16:35:13 +0000

    test

--- run_hmc_all_gpu.sh
+++ run_hmc_all_gpu.sh
@@ -10,7 +10,7 @@
 #SBATCH --mail-user=tian.li@port.ac.uk
 #SBATCH --mail-type=ALL
 #SBATCH --partition=gpu.q
-#SBATCH --exclude gpu0[1-2]
+#SBATCH --exclude gpu0[1]
 
 module load system
 module load anaconda3/2024.02
@@ -18,6 +18,9 @@ echo `module list`
 
 source /mnt/lustre2/shared_conda/envs/tianli/herculens_tian/bin/activate
 cd /users/tianli/LensedUniverse
+
+echo "[STEP] Run joint HMC"
+python -u hmc_scripts/run_joint_hmc.py
 
 echo "[STEP] Run lens+kinematic HMC"
 python -u hmc_scripts/run_lens_kin_hmc.py
@@ -31,5 +34,3 @@ python -u hmc_scripts/run_sne_hmc.py
 echo "[STEP] Run lensed quasar HMC"
 python -u hmc_scripts/run_quasar_hmc.py
 
-echo "[STEP] Run joint HMC"
-python -u hmc_scripts/run_joint_hmc.py
